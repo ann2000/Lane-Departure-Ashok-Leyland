b4e1484f1285c9ffada666359c59b65b4d05fe3e
Ann Rose Cherian <annrose@MacBook-Pro.local>
2021-04-27 17:29:51 +0530

    initial commit

---
+++ Lane_departure.py
@@ -0,0 +1,308 @@
+import cv2
+import numpy as np
+import os
+from scipy import optimize
+from matplotlib import pyplot as plt, cm, colors
+
+ym_per_pix = 30 / 720
+xm_per_pix = 3.7 / 720
+
+CWD_PATH = os.getcwd()
+
+def readVideo():
+
+    inpImage = cv2.VideoCapture(os.path.join(CWD_PATH, 'drive.mp4'))
+    return inpImage
+
+def processImage(inpImage):
+
+    hls = cv2.cvtColor(inpImage, cv2.COLOR_BGR2HLS)
+    lower_white = np.array([0, 160, 10])
+    upper_white = np.array([255, 255, 255])
+    mask = cv2.inRange(inpImage, lower_white, upper_white)
+    hls_result = cv2.bitwise_and(inpImage, inpImage, mask = mask)
+
+    gray = cv2.cvtColor(hls_result, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 160, 255, cv2.THRESH_BINARY)
+    blur = cv2.GaussianBlur(thresh,(3, 3), 0)
+    canny = cv2.Canny(blur, 40, 60)
+
+    return image, hls_result, gray, thresh, blur, canny
+
+def perspectiveWarp(inpImage):
+
+    img_size = (inpImage.shape[1], inpImage.shape[0])
+
+    src = np.float32([[590, 440],
+                      [690, 440],
+                      [200, 640],
+                      [1000, 640]])
+
+    dst = np.float32([[200, 0],
+                      [1200, 0],
+                      [200, 710],
+                      [1200, 710]])
+
+    matrix = cv2.getPerspectiveTransform(src, dst)
+    minv = cv2.getPerspectiveTransform(dst, src)
+    birdseye = cv2.warpPerspective(inpImage, matrix, img_size)
+    height, width = birdseye.shape[:2]
+    birdseyeLeft  = birdseye[0:height, 0:width // 2]
+    birdseyeRight = birdseye[0:height, width // 2:width]
+
+    return birdseye, birdseyeLeft, birdseyeRight, minv
+
+def plotHistogram(inpImage):
+
+    histogram = np.sum(inpImage[inpImage.shape[0] // 2:, :], axis = 0)
+
+    midpoint = np.int(histogram.shape[0] / 2)
+    leftxBase = np.argmax(histogram[:midpoint])
+    rightxBase = np.argmax(histogram[midpoint:]) + midpoint
+
+    plt.xlabel("Image X Coordinates")
+    plt.ylabel("Number of White Pixels")
+
+    return histogram, leftxBase, rightxBase
+
+def slide_window_search(binary_warped, histogram):
+
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
+    midpoint = np.int(histogram.shape[0] / 2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    nwindows = 9
+    window_height = np.int(binary_warped.shape[0] / nwindows)
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    margin = 100
+    minpix = 50
+    left_lane_inds = []
+    right_lane_inds = []
+
+    for window in range(nwindows):
+        win_y_low = binary_warped.shape[0] - (window + 1) * window_height
+        win_y_high = binary_warped.shape[0] - window * window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high),
+        (0,255,0), 2)
+        cv2.rectangle(out_img, (win_xright_low,win_y_low), (win_xright_high,win_y_high),
+        (0,255,0), 2)
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+        (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+        (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0])
+    left_fitx = left_fit[0] * ploty**2 + left_fit[1] * ploty + left_fit[2]
+    right_fitx = right_fit[0] * ploty**2 + right_fit[1] * ploty + right_fit[2]
+
+    ltx = np.trunc(left_fitx)
+    rtx = np.trunc(right_fitx)
+    plt.plot(right_fitx)
+    #plt.show()
+
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+    #plt.imshow(out_img)
+    plt.plot(left_fitx,  ploty, color = 'yellow')
+    plt.plot(right_fitx, ploty, color = 'yellow')
+    plt.xlim(0, 1280)
+    plt.ylim(720, 0)
+
+    return ploty, left_fit, right_fit, ltx, rtx
+
+def general_search(binary_warped, left_fit, right_fit):
+
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    margin = 100
+    left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy +
+    left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) +
+    left_fit[1]*nonzeroy + left_fit[2] + margin)))
+
+    right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy +
+    right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) +
+    right_fit[1]*nonzeroy + right_fit[2] + margin)))
+
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0])
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    window_img = np.zeros_like(out_img)
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+    left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+    left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin,
+                                  ploty])))])
+    left_line_pts = np.hstack((left_line_window1, left_line_window2))
+    right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+    right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, ploty])))])
+    right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+    cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
+    cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
+    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+
+    #plt.imshow(result)
+    plt.plot(left_fitx,  ploty, color = 'yellow')
+    plt.plot(right_fitx, ploty, color = 'yellow')
+    plt.xlim(0, 1280)
+    plt.ylim(720, 0)
+
+    ret = {}
+    ret['leftx'] = leftx
+    ret['rightx'] = rightx
+    ret['left_fitx'] = left_fitx
+    ret['right_fitx'] = right_fitx
+    ret['ploty'] = ploty
+
+    return ret
+
+def measure_lane_curvature(ploty, leftx, rightx):
+
+    leftx = leftx[::-1] 
+    rightx = rightx[::-1]  
+
+    y_eval = np.max(ploty)
+    left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+    left_curverad  = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+
+    if leftx[0] - leftx[-1] > 60:
+        curve_direction = 'Left Curve'
+    elif leftx[-1] - leftx[0] > 60:
+        curve_direction = 'Right Curve'
+    else:
+        curve_direction = 'Straight'
+
+    return (left_curverad + right_curverad) / 2.0, curve_direction
+
+def draw_lane_lines(original_image, warped_image, Minv, draw_info):
+
+    leftx = draw_info['leftx']
+    rightx = draw_info['rightx']
+    left_fitx = draw_info['left_fitx']
+    right_fitx = draw_info['right_fitx']
+    ploty = draw_info['ploty']
+
+    warp_zero = np.zeros_like(warped_image).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    mean_x = np.mean((left_fitx, right_fitx), axis=0)
+    pts_mean = np.array([np.flipud(np.transpose(np.vstack([mean_x, ploty])))])
+
+    cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
+    cv2.fillPoly(color_warp, np.int_([pts_mean]), (0, 255, 255))
+
+    newwarp = cv2.warpPerspective(color_warp, Minv, (original_image.shape[1], original_image.shape[0]))
+    result = cv2.addWeighted(original_image, 1, newwarp, 0.3, 0)
+
+    return pts_mean, result
+
+def offCenter(meanPts, inpFrame):
+
+    mpts = meanPts[-1][-1][-2].astype(int)
+    pixelDeviation = inpFrame.shape[1] / 2 - abs(mpts)
+    deviation = pixelDeviation * xm_per_pix
+    direction = "left" if deviation < 0 else "right"
+
+    return deviation, direction
+
+def addText(img, radius, direction, deviation, devDirection):
+
+    font = cv2.FONT_HERSHEY_TRIPLEX
+
+    if (direction != 'Straight'):
+        text = 'Radius of Curvature: ' + '{:04.0f}'.format(radius) + 'm'
+        text1 = 'Curve Direction: ' + (direction)
+
+    else:
+        text = 'Radius of Curvature: ' + 'N/A'
+        text1 = 'Curve Direction: ' + (direction)
+
+    cv2.putText(img, text , (50,100), font, 0.8, (0,100, 200), 2, cv2.LINE_AA)
+    cv2.putText(img, text1, (50,150), font, 0.8, (0,100, 200), 2, cv2.LINE_AA)
+
+    deviation_text = 'Off Center: ' + str(round(abs(deviation), 3)) + 'm' + ' to the ' + devDirection
+    cv2.putText(img, deviation_text, (50, 200), cv2.FONT_HERSHEY_TRIPLEX, 0.8, (0,100, 200), 2, cv2.LINE_AA)
+
+    return img
+
+image = readVideo()
+
+while True:
+
+    _, frame = image.read()
+
+    birdView, birdViewL, birdViewR, minverse = perspectiveWarp(frame)
+    img, hls, grayscale, thresh, blur, canny = processImage(birdView)
+    imgL, hlsL, grayscaleL, threshL, blurL, cannyL = processImage(birdViewL)
+    imgR, hlsR, grayscaleR, threshR, blurR, cannyR = processImage(birdViewR)
+    hist, leftBase, rightBase = plotHistogram(thresh)
+    #print(rightBase - leftBase)
+    plt.plot(hist)
+    #plt.show()
+
+
+    ploty, left_fit, right_fit, left_fitx, right_fitx = slide_window_search(thresh, hist)
+    plt.plot(left_fit)
+    #plt.show()
+
+
+    draw_info = general_search(thresh, left_fit, right_fit)
+    #plt.show()
+
+    curveRad, curveDir = measure_lane_curvature(ploty, left_fitx, right_fitx)
+    meanPts, result = draw_lane_lines(frame, thresh, minverse, draw_info)
+
+    deviation, directionDev = offCenter(meanPts, frame)
+
+    finalImg = addText(result, curveRad, curveDir, deviation, directionDev)
+
+    cv2.imshow("Final", finalImg)
+
+    if cv2.waitKey(1) == 13:
+        break
+
+image.release()
+cv2.destroyAllWindows()
